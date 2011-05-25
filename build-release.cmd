41e0dc7bad24d86234a3fdcabbf2d0dc548e6a3d
Kim Grasman <kim.grasman@gmail.com>
2011-05-25 16:59:20 +0200

    build-release.cmd now takes a tag name and generates version info from it, for traceability between binary and the tag it was built from.

--- build-release.cmd
+++ build-release.cmd
@@ -3,6 +3,31 @@
 REM This build script is not necessarily for general consumption.
 REM We use it internally to build fitSharp for .NET 3.5 and 4.0, in Release and targetted for x86, which
 REM happens to be a local requirement for our system under test.
+setlocal
+
+set TAG_NAME=%1
+if "%TAG_NAME%"=="" (
+  echo You must set a git tag before building, and use that tag name for traceability.
+  echo.
+  echo Usage: build-release.cmd ^<git tag name^>
+  exit /B 1
+) else (
+  REM Generate InternalVersionInfo.cs based on tag name
+  call :WriteInternalVersionInfoHeader
+  echo [assembly: AssemblyInformationalVersion^("Built from http://github.com/kimgr/fitsharp/tree/%TAG_NAME%"^)] >> source\InternalVersionInfo.cs
+)
 
 call build.cmd /p:TargetFrameworkVersion=v3.5;PlatformTarget=x86;Config=Release
 call build.cmd /p:TargetFrameworkVersion=v4.0;PlatformTarget=x86;Config=Release
+set EXIT_CODE=%ERRORLEVEL%
+
+REM Reset InternalVersionInfo.cs to empty state
+call :WriteInternalVersionInfoHeader
+
+exit /B %EXIT_CODE%
+
+:WriteInternalVersionInfoHeader
+  echo using System.Reflection; > source\InternalVersionInfo.cs
+  echo using System.Runtime.InteropServices; >> source\InternalVersionInfo.cs
+  echo. >> source\InternalVersionInfo.cs
+  goto :eof
